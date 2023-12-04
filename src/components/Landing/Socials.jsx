fb54f85a52830bd0d7e0acd6ab88780c99e9b7c0
Daniel Irwin <danielirwin99@gmail.com>
2023-12-04 17:29:03 +1100

    Socials

--- src/components/Landing/Socials.jsx
+++ src/components/Landing/Socials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-import { BsFacebook, BsInstagram } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
 
 const Socials = () => {
   return (
@@ -15,21 +16,33 @@ const Socials = () => {
         <div className="mt-8 flex space-x-10 items-center">
           <a
             href="https://www.google.com/search?q=total+exterior+cleaning&rlz=1C1CHBF_en-GBAU937AU937&oq=total+exterior+cleaning&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDwyBggCEEUYPDIGCAMQRRg80gEINDAzMWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
-            className="w-12 h-12 hover:scale-110 transition duration-100 ease-in-out"
+            className="w-11 hover:scale-110 transition duration-100 ease-in-out"
           >
-            <FcGoogle className="h-full w-full text-4xl" />
+            <FcGoogle className="h-full w-full " />
           </a>
           <a
-            className="w-10 h-10 hover:scale-110 transition duration-100 ease-in-out"
+            className="w-10 hover:scale-110 transition duration-100 ease-in-out"
             href="https://www.instagram.com/totalexteriorcleaning_/"
           >
-            <BsInstagram className="h-full w-full text-4xl text-rose-700" />
+            <BsInstagram className="h-full w-full  text-rose-700" />
           </a>
           <a
-            className="w-10 h-10 hover:scale-110 transition duration-100 ease-in-out"
+            className="w-10 hover:scale-110 transition duration-100 ease-in-out"
             href="https://www.facebook.com/profile.php?id=61550873473201"
           >
-            <BsFacebook className="h-full w-full text-3xl text-blue-800" />
+            <BsFacebook className="h-full w-full  text-blue-800" />
+          </a>
+          <a
+            className="w-12 hover:scale-110 transition duration-100 ease-in-out"
+            href="https://www.youtube.com/channel/UCsgfjljDcBb-LcDYvHt-D3w"
+          >
+            <BsYoutube className="h-full w-full text-red-700" />
+          </a>
+          <a
+            className="w-10 hover:scale-110 transition duration-100 ease-in-out"
+            href="https://www.tiktok.com/@totalexteriorco"
+          >
+            <FaTiktok className="h-full w-full " />
           </a>
         </div>
       </div>
