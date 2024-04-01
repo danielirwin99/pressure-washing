e0a3ba9b5aad98778558d943dbe519a3f013c8a3
daniel <danielirwin99@gmail.com>
2024-04-01 13:24:16 +1100

    new data

--- src/data/Testimonials.js
+++ src/data/Testimonials.js
@@ -5,41 +5,42 @@ export const data = [
     name: "John Englezos",
     img: user,
     review:
-      "Great, fast service, the driveway looks brand new! Easy to organise.",
+      "❝ Great, fast service, the driveway looks brand new! Easy to organise. ❞",
   },
   {
     name: "Jordan Dmello",
     img: user,
     review:
-      "Sean and Daniel are highly professional and do an amazing job, best in the business. Highly recommend this company",
+      "❝ Sean and Daniel are highly professional and do an amazing job, best in the business. Highly recommend this company ❞",
   },
   {
     name: "Declan",
     img: user,
     review:
-      "Sean and Daniel did an amazing job cleaning the windows and tiles of my apartment balcony. They cleaned up after themselves and had great communication.",
+      "❝ Sean and Daniel did an amazing job cleaning the windows and tiles of my apartment balcony. They cleaned up after themselves and had great communication. ❞",
   },
   {
     name: "Joshua Swan",
     img: user,
-    review: "Friendly, reliable and quick service. I would highly recommend.",
+    review:
+      "❝ Friendly, reliable and quick service. I would highly recommend. ❞",
   },
   {
     name: "Amelia Bennett",
     img: user,
     review:
-      "Total Exterior Cleaning exceeded my expectations with their pressure cleaning expertise. My home looks brand new—prompt, professional, and highly recommended!",
+      " ❝ Total Exterior Cleaning exceeded my expectations with their pressure cleaning expertise. My home looks brand new—prompt, professional, and highly recommended! ❞ ",
   },
   {
     name: "Theresa Chisari",
     img: user,
     review:
-      "Thank you to the team at TOTAL EXTERIOR CLEANING for their incredible job today, they went above and beyond with our cleaning job. ",
+      "❝ Thank you to the team at TOTAL EXTERIOR CLEANING for their incredible job today, they went above and beyond with our cleaning job. ❞",
   },
   {
     name: "Paul Para",
     img: user,
     review:
-      "Had my driveway cleaned, they did a great job! It looked brand new again. Very friendly also, would highly recommend.",
+      "❝ Had my driveway cleaned, they did a great job! It looked brand new again. Very friendly also, would highly recommend. ❞",
   },
 ];
