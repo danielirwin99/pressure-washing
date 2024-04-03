23156977b9dda4be63576395c74968f0f2098aa6
daniel <danielirwin99@gmail.com>
2024-04-03 16:47:38 +1100

    asdasd

--- src/App.js
+++ src/App.js
@@ -17,76 +17,77 @@ import Gallery from "./pages/Gallery";
 import SoftWashing from "./pages/SoftWashing";
 import AboutUs from "./pages/AboutUs";
 import ServiceAreas from "./pages/ServiceAreas";
+import ScrollToTop from "./components/UI/ScrollToTop";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-  {
-    path: "/about-us",
-    element: <AboutUs />,
-  },
-  {
-    path: "/pressure-washing",
-    element: <PressureWashing />,
-  },
-  {
-    path: "/gutter-cleaning",
-    element: <GutterCleaning />,
-  },
-  {
-    path: "/areas-of-service",
-    element: <ServiceAreas />,
-  },
-  {
-    path: "/window-cleaning",
-    element: <WindowCleaning />,
-  },
-  {
-    path: "/sealing",
-    element: <DrivewaySealing />,
-  },
-  {
-    path: "/gallery",
-    element: <Gallery />,
-  },
-  {
-    path: "/soft-washing",
-    element: <SoftWashing />,
-  },
-  {
-    path: "/contact-us",
-    element: <Contact />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Landing />,
+//   },
+//   {
+//     path: "/about-us",
+//     element: <AboutUs />,
+//   },
+//   {
+//     path: "/pressure-washing",
+//     element: <PressureWashing />,
+//   },
+//   {
+//     path: "/gutter-cleaning",
+//     element: <GutterCleaning />,
+//   },
+//   {
+//     path: "/areas-of-service",
+//     element: <ServiceAreas />,
+//   },
+//   {
+//     path: "/window-cleaning",
+//     element: <WindowCleaning />,
+//   },
+//   {
+//     path: "/sealing",
+//     element: <DrivewaySealing />,
+//   },
+//   {
+//     path: "/gallery",
+//     element: <Gallery />,
+//   },
+//   {
+//     path: "/soft-washing",
+//     element: <SoftWashing />,
+//   },
+//   {
+//     path: "/contact-us",
+//     element: <Contact />,
+//   },
+//   {
+//     path: "*",
+//     element: <Error />,
+//   },
+// ]);
 
 function App() {
   return (
     <div>
-      {/* <ScrollRestoration /> */}
-      <RouterProvider router={router} />
-      {/* OLD ROUTER WAY */}
+      {/* NEW ROUTER WAY */}
+      {/* <RouterProvider router={router} /> */}
       <>
-        {/* <Router>
-        <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/pressure-washing" element={<PressureWashing />} />
-        <Route path="/gutter-cleaning" element={<GutterCleaning />} />
-        <Route path="/areas-of-service" element={<ServiceAreas />} />
-        <Route path="/window-cleaning" element={<WindowCleaning />} />
-        <Route path="/sealing" element={<DrivewaySealing />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/soft-washing" element={<SoftWashing />} />
-        <Route path="/contact-us" element={<Contact />} />
-        <Route path="*" element={<Error />} />
-        </Routes>
-      </Router> */}
+        <Router>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/pressure-washing" element={<PressureWashing />} />
+            <Route path="/gutter-cleaning" element={<GutterCleaning />} />
+            <Route path="/areas-of-service" element={<ServiceAreas />} />
+            <Route path="/window-cleaning" element={<WindowCleaning />} />
+            <Route path="/sealing" element={<DrivewaySealing />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/soft-washing" element={<SoftWashing />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
       </>
     </div>
   );
