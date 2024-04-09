2b097b5e4b8d99f6589ebe04ec2cca16ada0f119
daniel <danielirwin99@gmail.com>
2024-04-10 00:05:25 +1000

    aaa

--- src/pages/PressureWashing.jsx
+++ src/pages/PressureWashing.jsx
@@ -64,7 +64,7 @@ const PressureWashing = () => {
         data-aos-easing="ease-in-out"
         className=" text-center mx-5 sm:text-start"
       >
-        <div className=" lg:mx-10 pt-10 mb-5  lg:mr-6 sm:flex flex-wrap lg:flex-nowrap lg:space-x-4">
+        <div className=" lg:mx-10 pt-14 mb-5  lg:mr-6 sm:flex flex-wrap lg:flex-nowrap lg:space-x-4">
           <div className="lg:w-[60%] space-y-8  lg:mx-6 lg:pr-8">
             <h1 className="text-3xl sm:text-5xl font-bold text-shadow-subtle sub-headers">
               Refresh Your Home with High Pressure Cleaning.
