e6edacccf69df7dbc91bf22434d06e4ec197c128
daniel <danielirwin99@gmail.com>
2024-04-09 13:56:37 +1000

    aaa

--- src/pages/PressureWashing.jsx
+++ src/pages/PressureWashing.jsx
@@ -68,7 +68,7 @@ const PressureWashing = () => {
         data-aos-easing="ease-in-out"
         className=" text-center mx-5 sm:text-start"
       >
-        <div className=" mx-6 lg:mx-10 pt-10 mb-5  lg:mr-6 sm:flex flex-wrap lg:flex-nowrap lg:space-x-4">
+        <div className=" lg:mx-10 pt-10 mb-5  lg:mr-6 sm:flex flex-wrap lg:flex-nowrap lg:space-x-4">
           <div className="lg:w-[60%] space-y-8  lg:mx-6 lg:pr-8">
             <h1 className="text-3xl sm:text-5xl font-bold text-shadow-subtle sub-headers">
               Refresh Your Home with High Pressure Cleaning.
@@ -150,7 +150,7 @@ const PressureWashing = () => {
             data-aos="fade-up"
             data-aos-duration="500"
             data-aos-easing="ease-in-out"
-            className="flex flex-col mx-3 sm:mx-0 sm:flex-row lg:flex-col my-10 lg:mt-0 lg:space-y-5 sm:space-x-4 lg:space-x-0 justify-center items-center lg:w-[40%]"
+            className="flex flex-col sm:mx-0 sm:flex-row lg:flex-col my-10 lg:mt-0 lg:space-y-5 sm:space-x-4 lg:space-x-0 justify-center items-center lg:w-[40%]"
           >
             {slice.map((image) => (
               <div className="w-full sm:w-1/2 lg:w-full lg:h-[400px] flex overflow-hidden rounded-sm">
@@ -206,7 +206,7 @@ const PressureWashing = () => {
       <div
         data-aos="fade-in"
         data-aos-easing="ease-in-out"
-        className=" mb-20 py-2 bg-gray-50 border-t-2 border-b-2 "
+        className=" mb-20 py-2  bg-gray-50 border-t-2 border-b-2 "
       >
         <Socials />
       </div>
