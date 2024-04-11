2e392057f801c581354598a2c395d25c717769e7
daniel <danielirwin99@gmail.com>
2024-04-11 19:21:12 +1000

    aaa

--- src/pages/PressureWashing.jsx
+++ src/pages/PressureWashing.jsx
@@ -73,7 +73,7 @@ const PressureWashing = () => {
                 alt="Background Image"
               />
             </figure>
-            <div className="pt-[150px] md:pt-[300px] md:h-[100vh] h-[700px] flex flex-col mx-4 sm:pr-0 lg:pl-32 space-y-14 sm:space-y-20">
+            <div className="pt-[150px] md:pt-[300px] text-center md:h-[100vh] h-[700px] flex flex-col mx-4 sm:pr-0  space-y-14 sm:space-y-20">
               <h1 className="text-white sub-headers uppercase text-4xl md:text-6xl tracking-wider font-bold text-shadow">
                 High Pressure Cleaning Sydney
               </h1>
