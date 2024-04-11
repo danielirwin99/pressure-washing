883e03b94fcb04ba6f53342b27021418a68cb1df
daniel <danielirwin99@gmail.com>
2024-04-11 13:52:59 +1000

    aaa

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const filePath = path.resolve(__dirname, "./build", "index.html");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    data = data
+      .replace(
+        /__TITLE__/g,
+        "Professional Pressure Cleaning - Total Exterior Cleaning"
+      )
+      .replace(
+        /__DESCRIPTION__/g,
+        "Sydney's Best Pressure Cleaners. Professional Pressure Washers specialising in high or soft pressure washing. Get Your Quote Now"
+      );
+
+    res.send(data);
+  });
+});
+
+app.get("/pressure-washing", (req, res) => {
+  const filePath = path.resolve(__dirname, "./build", "index.html");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    data = data
+      .replace(
+        /__TITLE__/g,
+        "Professional Pressure Washing | House Washing Sydney"
+      )
+      .replace(
+        /__DESCRIPTION__/g,
+        "Sydney's Best Pressure Cleaners. Professional Pressure Washers specialising in high or soft pressure washing. Get Your Quote Now"
+      );
+
+    res.send(data);
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, "./build")));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
