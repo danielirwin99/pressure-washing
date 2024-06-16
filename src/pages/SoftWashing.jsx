33f5c13469d5793eef347edf762f29735a5c5c07
daniel <danielirwin99@gmail.com>
2024-06-16 14:11:10 +1000

    aaa

--- src/pages/SoftWashing.jsx
+++ src/pages/SoftWashing.jsx
@@ -416,7 +416,7 @@ const HouseWashing = () => {
               data-aos="fade-up"
               data-aos-duration="500"
               data-aos-easing="ease-in-out"
-              className="flex flex-col sm:mx-0 sm:flex-row lg:flex-col my-10 lg:mt-0 lg:space-y-5 sm:space-x-4 lg:space-x-0 justify-center items-center lg:w-[45%]"
+              className="flex flex-col sm:mx-0 sm:flex-row lg:flex-col my-6 lg:mt-0 lg:space-y-5 sm:space-x-4 lg:space-x-0 justify-center items-center lg:w-[45%]"
             >
               {slice.map((image) => (
                 <div className=" sm:w-1/2 w-[90%] h-[350px] lg:w-full lg:h-[400px] 4xl:h-[500px] flex overflow-hidden rounded-sm">
